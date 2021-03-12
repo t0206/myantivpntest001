55ff3a0f7815487871503a4faa25445fbcec653e
t0206 <70625710+t0206@users.noreply.github.com>
2021-03-12 21:28:56 +0900

    Add files via upload

---
+++ src/main/java/vpnblocker/novpn/pluginnovpn/novpn/NoVPN/utils/ConfigManager.java
@@ -0,0 +1,4 @@
+package vpnblocker.novpn.pluginnovpn.novpn.NoVPN.utils;
+
+public class ConfigManager {
+}
---
+++ src/main/java/vpnblocker/novpn/pluginnovpn/novpn/NoVPN/utils/JsonObjectUtil.java
@@ -0,0 +1,21 @@
+package vpnblocker.novpn.pluginnovpn.novpn.NoVPN.utils;
+
+import okhttp3.*;
+import org.json.simple.JSONObject;
+
+import java.io.IOException;
+
+public class JsonObjectUtil{
+
+    public static String getString(String jsonObject,String getString) throws IOException {
+        String url = "http://www.casareal.co.jp";
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(url).build();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        ResponseBody body = response.body();
+        JSONObject jobject = new JSONObject("");
+        return  jobject.get( "hoge" )+"";
+    }
+}
